a5f9b3e53dd291cad2ec53e13c1c81024f13d207
johannesTbz <johanneslind02@gmail.com>
2024-09-27 13:34:10 +0200

    changed functionality of LED to only turn on/off on command message via mqtt

--- arduino_IDE_code/arduino_IDE_code.ino
+++ arduino_IDE_code/arduino_IDE_code.ino
@@ -7,8 +7,8 @@ const char *pass = "Bondvagen46!";
 const int ledPin = 18;
 
 //MQTT BROKER CREDENTIALS
-const char *mqtt_broker= "broker.emqx.io";
-const int mqtt_port= 1883;
+const char *mqtt_broker = "broker.emqx.io";
+const int mqtt_port = 1883;
 const char *topic = "emqx/esp32/led";
 const char *mqtt_username = "emqx";
 const char *mqtt_password = "123abc";
@@ -21,12 +21,12 @@ void setup() {
 
   //sätter led pin till output
   pinMode(ledPin, OUTPUT);
-  
+
   Serial.println("Starting LED control...");
 
   WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED){
-    delay (1000);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
     Serial.println("connecting to wifi...");
   }
   Serial.println("Connected to the Wi-fi network");
@@ -35,46 +35,45 @@ void setup() {
   //connecting to a mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
-  while(!client.connected()){
+  while (!client.connected()) {
     String client_id = "mqttx_cc626830";
-    client_id +=String(WiFi.macAddress());
+    client_id += String(WiFi.macAddress());
     Serial.printf("The client %s is connecting to the MQTT broker\n", client_id.c_str());
-    if(client.connect(client_id.c_str(), mqtt_username, mqtt_password)){
-       Serial.println("EMQX MQTT broker connected");
-    }else{
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("EMQX MQTT broker connected");
+    } else {
       Serial.print("Failed with state");
       Serial.print(client.state());
       delay(2000);
     }
   }
-  client.publish(topic,"HI I'm esp32");
+  client.publish(topic, "HI I'm esp32");
   client.subscribe(topic);
 }
 
 void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message arrived in topic: ");
-    Serial.println(topic);
-    Serial.print("Message:");
-    for (int i = 0; i < length; i++) {
-        Serial.print((char) payload[i]);
-    }
-    Serial.println();
-    Serial.println("-----------------------");
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+
+  String message;
+  for (int i = 0; i < length; i++) {
+        message += (char) payload[i];
+  }
+  Serial.println(message);
+  Serial.println("-----------------------");
+
+  if (message.equalsIgnoreCase("on")) {
+    digitalWrite(ledPin, HIGH);
+    Serial.println("LED turned ON");
+  } else if (message.equalsIgnoreCase("off")) {
+    digitalWrite(ledPin, LOW);
+    Serial.println("LED turned OFF");
+  } else {
+    Serial.println("Unknown command");
+  }
 }
 
 void loop() {
   client.loop();
-   
-   // Turn the LED ON
-  digitalWrite(ledPin, HIGH);
-  Serial.println("LED is ON");
-  // Wait for 1 second (1000 milliseconds)
-  delay(1000);
-
-  // Turn the LED OFF
-  digitalWrite(ledPin, LOW);
-  Serial.println("LED is OFF");
-  // Wait for 1 second
-  delay(1000);
-
 }
