00a01b4f43bb85aa61939dcd31e771550eefb686
Kechida98 <kechida.ok@gmail.com>
2024-10-15 16:15:31 +0200

    Stopped sending data contiunously

--- arduino_IDE_code/arduino_IDE_code.ino
+++ arduino_IDE_code/arduino_IDE_code.ino
@@ -6,6 +6,9 @@ const int LOADCELL_SCK_PIN = 19;
 
 HX711 scale;
 
+float previousWeight = 0;
+float threshold = 1.0;
+
 void setup() {
   Serial.begin(115200);
 
@@ -53,13 +56,44 @@ void setup() {
   Serial.println("Readings:");  
 }
 void loop() {
- Serial.print("one reading:\t");
-  Serial.print(scale.get_units(), 1);
-  Serial.print("\t| average:\t");
-  Serial.println(scale.get_units(100), 5);
+  //Raw reeading value because reading-tare/factor and we dont have factor,
+  //we will then get different raw reading values depending on the weight of the object
+  //that we can use for reading/object weight to get callobaration factor, to then put it in
+  //set_scale(callobarion factor) to convert data to human readable.
+  /*void loop() {
+  if(scale.is_ready()){
+    scale.set_scale();
+    Serial.println("Tare.. remove any weights from scale");
+    delay(5000);
+    scale.tare();
+    Serial.println("Tare done");
+    Serial.print("Place a known weight on the scale");
+    delay(5000);
+    long reading = scale.get_units(10);
+    Serial.print("Result: ");
+    Serial.println(reading);
+  }else{
+    Serial.println("HX711 NOT FOUND.");
+  }
+  delay(1000);*/
 
-  scale.power_down();             // put the ADC in sleep mode
-  delay(5000);
-  scale.power_up();
+ // Get the current weight reading
+  float currentWeight = scale.get_units(10);  // Average of 10 readings for stability
+
+  // Check if currentweight minus previousweight is greater then threshold.
+  if (abs(currentWeight - previousWeight) > threshold) {
+    Serial.print("Change detected. Weight:");
+    Serial.print(currentWeight, 1);  // Print current weight with 1 decimal
+    Serial.print(" g | Previous weight: "); 
+    Serial.print(previousWeight, 1);  // same as line 83
+    Serial.println(" g");
+
+    // Update the previous weight to currentWeight.
+    previousWeight = currentWeight;
+  }
+
+  scale.power_down();  // Put the ADC in sleep mode to save power
+  delay(5000);  // Wait for 5 seconds
+  scale.power_up();  // Power up the ADC
 }
 //Callobration är reading/vikten vi vet och reading det kommer vi få när vi gör Serial.println(reading);
